639c132d116a42ae75808d734b4720d455681c46
NeTR153 <39942793+NeTR153@users.noreply.github.com>
2018-06-13 12:50:37 +0300

    Update project.py

--- project.py
+++ project.py
@@ -63,8 +63,9 @@ def answer(chat_id, message):   # функция,обрабатывающая с
         send_message(chat_id, "Здравствуйте.")
         button(chat_id)
     elif message == "Обмен":
+        send_message(chat_id, "Эта функция в разработке")
         tradebuttons(chat_id)
-        # send_message(chat_id, "Эта функция в разработке")
+        
     elif message == "Кошелёк":
         send_message(chat_id, "Функция в разработке")
         # UserInfo = databasefuncSelect(chat_id)
@@ -79,7 +80,7 @@ def answer(chat_id, message):   # функция,обрабатывающая с
                #  databasefunc(chat_id, newaddr, 0, 0)
             # except:
                #  send_message(chat_id, "create_address problem")
-        # walletbuttons(chat_id, 0)  # заместо 0 поставить balance, который будет получаться из б.д.
+        walletbuttons(chat_id, 0)  # заместо 0 поставить balance, который будет получаться из б.д.
     elif message == "send":
         send_message(chat_id,"Эта функция пока не работает")
     elif message == "sendback":
